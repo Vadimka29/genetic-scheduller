068347e85efb07ee2265469d12a25ce824b461f9
Vadym Akymov <vadimka2995@gmail.com>
2016-05-03 15:35:29 +0300

    test folder refactoring

---
+++ src/test/java/com/redkite/algorithm/AlgorithmFactoryTest.java
@@ -0,0 +1,19 @@
+package com.redkite.algorithm;
+
+import com.redkite.algorithm.Algorithm;
+import com.redkite.algorithm.AlgorithmFactory;
+import com.redkite.algorithm.AlgorithmType;
+import org.junit.Test;
+
+/**
+ * Created by Vadym on 26.04.2016.
+ */
+
+public class AlgorithmFactoryTest {
+
+    @Test
+    public void testRetrieveAlgorithmRealization(){
+        Algorithm algorithm = (new AlgorithmFactory()).retrieveAlgorithmRealization(AlgorithmType.GENETIC_ALGORITHM);
+        algorithm.doCalculation();
+    }
+}
---
+++ src/test/java/com/redkite/algorithm/simannealing/SimulatedAnnealingTest.java
@@ -0,0 +1,16 @@
+package com.redkite.algorithm.simannealing;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Vadym on 27.04.2016.
+ */
+public class SimulatedAnnealingTest {
+
+    @Test
+    public void doCalculation() throws Exception {
+
+    }
+}
